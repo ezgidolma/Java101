4b046e6105baf96ff60ad35df579bd0fa2f47d87
ezgid <ezgidolma23@gmail.com>
2023-07-03 23:30:25 +0300

    .

---
+++ src/ArtikYil.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ArtikYil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int yil;
+        System.out.println("Yıl girin: ");
+        yil = input.nextInt();
+
+
+
+                if((yil%4==0)&& (yil%100!=0) || (yil%400==0)){
+                    System.out.println(yil + "artık bir yıldır");
+                }
+                else {
+                    System.out.println(yil + "artık bir yıl değilidr");
+                }
+
+
+
+    }
+}
