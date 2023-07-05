d5b4f8f5363ecb8134b104eab3761c3031279aff
ezgid <ezgidolma23@gmail.com>
2023-07-05 14:42:21 +0300

    .

---
+++ src/MukemmelSayi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int sayi,toplam=0;
+        System.out.println("Sayi girin: ");
+        sayi=input.nextInt();
+
+        for(int i=1;i<sayi;i++){
+            if(sayi%i==0)
+                toplam+=i;
+        }
+        if(sayi==toplam){
+            System.out.println(sayi+"Mükemmel sayıdır");
+        }
+        else {
+            System.out.println(sayi+"Mükemmel sayı değildir");
+        }
+    }
+}
